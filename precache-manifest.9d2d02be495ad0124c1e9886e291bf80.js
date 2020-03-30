8bb42877ac09a6621db2971f17d695077e73c9d7
Abhinav Arya <abhinav.arya@cerner.com>
2020-03-30 23:39:56 +0530

    Updates

---
+++ precache-manifest.9d2d02be495ad0124c1e9886e291bf80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa14cd3fc741402ceb801f07b9ef1b45",
+    "url": "/shopping-cart-app/index.html"
+  },
+  {
+    "revision": "53c4eacd9a5007319deb",
+    "url": "/shopping-cart-app/static/css/2.4b2a5115.chunk.css"
+  },
+  {
+    "revision": "3e6e1cf7f93bf998315d",
+    "url": "/shopping-cart-app/static/css/main.82895ba9.chunk.css"
+  },
+  {
+    "revision": "53c4eacd9a5007319deb",
+    "url": "/shopping-cart-app/static/js/2.fe363fa9.chunk.js"
+  },
+  {
+    "revision": "af367636e6d0b874e2906bd3c1897e11",
+    "url": "/shopping-cart-app/static/js/2.fe363fa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6e1cf7f93bf998315d",
+    "url": "/shopping-cart-app/static/js/main.a53b7935.chunk.js"
+  },
+  {
+    "revision": "bbc0bbc6dc864c747c3d",
+    "url": "/shopping-cart-app/static/js/runtime-main.1494f9f5.js"
+  }
+]);
